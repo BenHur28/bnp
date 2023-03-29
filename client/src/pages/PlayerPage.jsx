0fa34346da2a4b0a3146c5eae50510d7f417c323
Ben Hur <Bhur777@gmail.com>
2023-03-28 21:46:34 -0400

    finish playerpage template

--- client/src/pages/PlayerPage.jsx
+++ client/src/pages/PlayerPage.jsx
@@ -24,37 +24,25 @@ const PlayerPage = () => {
 			<div className="h-1/2 mt-10">
 				<iframe
 					className="mx-auto aspect-video rounded-2xl lg:w-1/3 lg:h-3/4 md:w-1/2 md:h-3/4 sm:w-1/2 sm: h-1/2 relative top-10"
-					src="https://www.youtube.com/embed/NZvmJFLH8Lk"
+					src={player.vodlinks[0]}
 					allowFullScreen
 				></iframe>
 			</div>
 			<div className="lg:w-1/2 md:w-1/2 sm: w-2/3 mx-auto text-left mb-6">
 				<h2 className="text-2xl text-center font-bold mb-3 ">Neutral</h2>
-				<ol className="list-outside list-disc">
-					<li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatem aspernatur fugit laudantium iste consequuntur perferendis</li>
-					<li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatem aspernatur fugit laudantium iste consequuntur perferendis</li>
-				</ol>
+				<ol className="list-outside list-disc">{player.neutral?.length > 0 && player.neutral.map((data, index) => <li key={index}>{data}</li>)}</ol>
 			</div>
 			<div className="lg:w-1/2 md:w-1/2 sm: w-2/3 mx-auto text-left mb-6">
 				<h2 className="text-2xl text-center font-bold mb-3 ">Offense</h2>
-				<ol className="list-outside list-disc">
-					<li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatem aspernatur fugit laudantium iste consequuntur perferendis</li>
-					<li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatem aspernatur fugit laudantium iste consequuntur perferendis</li>
-				</ol>
+				<ol className="list-outside list-disc">{player.neutral?.length > 0 && player.offense.map((data, index) => <li key={index}>{data}</li>)}</ol>
 			</div>
 			<div className="lg:w-1/2 md:w-1/2 sm: w-2/3 mx-auto text-left mb-6">
 				<h2 className="text-2xl text-center font-bold mb-3 ">Defense</h2>
-				<ol className="list-outside list-disc">
-					<li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatem aspernatur fugit laudantium iste consequuntur perferendis</li>
-					<li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatem aspernatur fugit laudantium iste consequuntur perferendis</li>
-				</ol>
+				<ol className="list-outside list-disc">{player.neutral?.length > 0 && player.defense.map((data, index) => <li key={index}>{data}</li>)}</ol>
 			</div>
 			<div className="lg:w-1/2 md:w-1/2 sm: w-2/3 mx-auto text-left mb-6 pb-10">
 				<h2 className="text-2xl text-center font-bold mb-3 ">GRD</h2>
-				<ol className="list-outside list-disc">
-					<li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatem aspernatur fugit laudantium iste consequuntur perferendis</li>
-					<li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatem aspernatur fugit laudantium iste consequuntur perferendis</li>
-				</ol>
+				<ol className="list-outside list-disc">{player.neutral?.length > 0 && player.grd.map((data, index) => <li key={index}>{data}</li>)}</ol>
 			</div>
 		</div>
 	);
