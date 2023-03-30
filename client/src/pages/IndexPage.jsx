8156c688ea9a1fbd2f88a4a990f0adae8698492b
Ben Hur <Bhur777@gmail.com>
2023-03-29 22:03:01 -0400

    finish styling char page. add funny gifs to homepage

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -5,6 +5,13 @@ const IndexPage = () => {
 	return (
 		<div>
 			<Navbar />
+			<div className="flex flex-col items-center w-5/6 mx-auto">
+				<h1 className="text-2xl font-bold my-4">You know the rules</h1>
+				<p className="text-lg mb-4 text-center">IF YOU LOSE, YOU CANT COME HOME</p>
+				<img className="rounded-2xl mb-4" src="https://media.discordapp.net/attachments/583753905620516866/1039966648125378651/the_voices.gif" alt="" />
+				<p className="text-lg mb-4 text-center">NOW GO WIN YOU DIAMOND IN THE ROUGH</p>
+				<img className="rounded-2xl" src="https://tenor.com/view/blue-lock-jinpachi-ego-ego-glasses-anime-glasses-gif-26886035.gif" alt="" />
+			</div>
 		</div>
 	);
 };
--- client/src/pages/SingleCharPage.jsx
+++ client/src/pages/SingleCharPage.jsx
@@ -20,19 +20,19 @@ const SingleCharPage = () => {
 		<div>
 			<Navbar />
 			<h1 className="text-3xl text-center font-bold mt-4"> {char?.name}</h1>
-			<div className="h-screen mt-10">
+			<div className="">
 				{char?.video.length > 0 &&
 					char.video.map((video, index) => (
 						<div className="h-1/2 mt-10" key={index}>
-							<h2 className="text-3xl text-center font-bold mt-4">{video.title}</h2>
+							<h2 className="text-3xl text-center font-bold my-4">{video.title}</h2>
 							<iframe
-								className="mx-auto aspect-video rounded-2xl lg:w-1/3 lg:h-3/4 md:w-1/2 md:h-3/4 sm:w-1/2 sm: h-1/2 relative top-10"
+								className="mx-auto aspect-video rounded-2xl lg:w-1/3 lg:h-3/4 md:w-1/2 md:h-3/4 sm:w-1/2 sm:h-1/2 max-sm:w-3/4"
 								src={video.link}
 								allowFullScreen
 							></iframe>
-							<div className="lg:w-1/3 md:w-1/2 sm: w-2/3 mx-auto text-left mt-20">
+							<div className="lg:w-1/3 md:w-1/2 sm: w-2/3 mx-auto text-left mt-4">
 								<h2 className="text-xl text-center font-bold mb-3 ">Description</h2>
-								<ol className="list-outside list-disc">
+								<ol className="list-outside list-disc mb-6">
 									<li>{video.description}</li>
 								</ol>
 							</div>
